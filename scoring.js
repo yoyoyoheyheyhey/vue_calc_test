d949a50a12ea4ee68aab663bf2ab95a60060608e
Yohei Goto <yohei_goto@diveintocode.jp>
2020-09-23 21:58:24 +0900

    :fu: indent

--- scoring.js
+++ scoring.js
@@ -1,37 +1,37 @@
 new Vue({
   el: "#app",
   data: {
-  	subjects: {
-    	national_language: { score: 0, ja: "国語" },
+    subjects: {
+      national_language: { score: 0, ja: "国語" },
       english:  { score: 0, ja: "英語" },
       mathematics: { score: 0, ja: "数学" },
       science: { score: 0, ja: "理科" },
       society: { score: 0, ja: "社会" },
     },
     resultShowCase: {
-    	rank: false,
+      rank: false,
       judgement: false,
       final: false,
     },
   },
   methods: {
-  	display(key) {
-    	this.resultShowCase[key] = !this.resultShowCase[key];
+    display(key) {
+      this.resultShowCase[key] = !this.resultShowCase[key];
     },
   },
   computed: {
-  	sum() {
+    sum() {
       return Object
-      	.keys(this.subjects)
+        .keys(this.subjects)
         .map((key) => this.subjects[key]['score'])
         .reduce((prev, next) => +prev + +next)
     },
     average() {
-    	return this.sum / Object.keys(this.subjects).length;
+      return this.sum / Object.keys(this.subjects).length;
     },
     rank() {
-    	const avg = this.average;
-    	if (avg >= 80) {
+      const avg = this.average;
+      if (avg >= 80) {
         return 'A';
       } else if (avg >= 60) {
         return 'B';
@@ -42,15 +42,15 @@ new Vue({
       }
     },
     judgement() {
-    	const isFailed = Object
-      	.keys(this.subjects)
+      const isFailed = Object
+        .keys(this.subjects)
         .some((key) => {
-      		return this.subjects[key]['score'] < 60;
-      	});
-			return isFailed ? "不合格" : "合格";
+          return this.subjects[key]['score'] < 60;
+        });
+      return isFailed ? "不合格" : "合格";
     },
     final() {
-    	return `<label id="alert-indicate" class="alert alert-info">あなたの成績は${this.rank}です。${this.judgement}です</label>`
+      return `<label id="alert-indicate" class="alert alert-info">あなたの成績は${this.rank}です。${this.judgement}です</label>`
     }
   }
 })
